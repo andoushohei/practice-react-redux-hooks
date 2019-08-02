c29add9bc2ba4f82b319aa2f03c45f5d1b8360e4
ANDO Shohei <andoushohei@realglobe.jp>
2019-08-02 17:04:43 +0900

    remove App.css

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import {Divider,Header} from 'semantic-ui-react'
 import Counter from './containers/Counter';
 import Counter2 from './containers/Counter2';
-import './App.css';
 
 const App: React.FC = () => (
   <>
